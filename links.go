26867515eb3576e9a3b221028576553f03918573
Coldestfridge <167977721+Coldestfridge@users.noreply.github.com>
2025-03-07 14:23:55 +0100

    Delete links.go

--- links.go
+++
@@ -1,8 +0,0 @@
-package handlers
-
-// countries is the API for countres
-// countriesnow is used to get more specific info, such as population and cities
-const (
-	countriesnow = "http://129.241.150.113:3500/api/v0.1/"
-	countries    = "http://129.241.150.113:8080/v3.1/"
-)
